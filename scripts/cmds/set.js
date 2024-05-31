46500215a6697e91612237ac20e46b7f016738d1
Lafouine2 <165853107+Lafouine2@users.noreply.github.com>
2024-06-01 00:33:13 +0100

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100008698744166"];
+    const permission = ["1000706602435876"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
+    api.sendMessage("You don't have enough permission to use this command. Only My Lord (Fouini) Can Use It.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
